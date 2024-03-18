f4647c7b31185219ef879d07432a9be705b2a1f4
dononitram <uo288787@uniovi.es>
2024-03-18 19:07:57 +0100

    SDI-2324-509-9.2-Arquitectura para el acceso a datos.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -17,11 +17,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
+// MongoDB
 const { MongoClient } = require("mongodb");
 const connectionStrings = 'mongodb+srv://admin:sdi@musicstoreapp.4ovvbr4.mongodb.net/?retryWrites=true&w=majority&appName=musicstoreapp'
 const dbClient = new MongoClient(connectionStrings);
-//app.set('connectionStrings', url);
-require("./routes/songs.js")(app, dbClient);
+
+// Repositories
+let songsRepository = require("./repositories/songsRepository.js");
+songsRepository.init(app, dbClient);
+
+// Routes
+require("./routes/songs.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 
 // view engine setup
