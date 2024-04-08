2dd6b140e48bf7664de723df40893e4f76a0a806
dononitram <uo288787@uniovi.es>
2024-04-08 23:21:04 +0200

    SDI-2324-509-11.1-SW API REST CRUD Canciones.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -70,6 +70,7 @@ songsRepository.init(app, dbClient);
 require("./routes/users.js")(app, usersRepository);
 require("./routes/songs/favorites.js")(app, favouritesRepository, songsRepository);
 require("./routes/songs.js")(app, songsRepository);
+require("./routes/api/songsAPIv1.0.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 
 // view engine setup
